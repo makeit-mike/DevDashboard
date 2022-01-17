14669822e667e93e3c79005cba447e2d876125e0
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-17 11:39:40 -0600

    Adding Code Review page and todo list.

--- DevDashboard/Classes/Extensions.cs
+++ DevDashboard/Classes/Extensions.cs
@@ -33,5 +33,7 @@ namespace DevDashboard.Classes
                 return defualt;
             }
         }
+
+
     }
 }
---
+++ DevDashboard/Pages/CodeReviews.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace DevDashboard.Pages
+{
+    public partial class CodeReviews : ComponentBase 
+    {
+        public enum ReviewStatus { InProg, Approved, Staged, Released }
+
+        private CodeReview[]? codeReviews;
+
+        protected override async Task OnInitializedAsync()
+        {
+            codeReviews = await Http.GetFromJsonAsync<CodeReview[]>("sample-data/reviews.json");
+        }
+
+        public class CodeReview
+        {
+            public int ID { get; set; }
+
+            public string Application { get; set; }
+
+            public string ReviewDate { get; set; } //ToDo: Make these work as DateTimes
+            public string ReleaseDate { get; set; } //ToDo: Make these work as DateTimes
+
+            public string Approver1 { get; set; }
+            public string Approver2 { get; set; }
+
+            public string Status { get; set; } //ToDo: Make these work as ReviewStatus enum type.
+
+
+            // Needs implementing.
+            public int FailCount { get; set; } = 0;
+            public bool ContainsSQL { get; set; } = false;
+        }
+
+
+        /*
+            Using JSON Generator.
+                '{{repeat(5, 7)}}',
+                {
+                    id: '{{index()}}',
+
+                    application: '{{random("Dashboard", "ToDoApp", "Tracker")}}',
+
+                    ReviewDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+
+                    ReleaseDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+
+                    approver1: '{{firstName()}} {{surname()}}',
+
+                    approver2: '{{firstName()}} {{surname()}}',
+
+                    status: '{{random("InProg", "Approved", "Released")}}'
+                }
+        */
+
+    }
+}
+
+
