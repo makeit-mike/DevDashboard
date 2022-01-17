1551decd93d6244a464781e8f61e16dd4d23e0d6
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-17 11:51:28 -0600

    adding code snippet codebehind

---
+++ DevDashboard/Shared/CodeSnippet.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http.Json;
+
+namespace DevDashboard.Shared
+{
+    public partial class CodeSnippet : ComponentBase
+    {
+        [Inject] private IJSRuntime _js { get; set; } //adding JS
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public string Language { get; set; } = "csharp";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            Console.WriteLine("Calling InvokeVoidAsync.");
+
+            await _js.InvokeVoidAsync("highlightSnippet");
+        }
+    }
+}
