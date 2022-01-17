56b599a13d14aaca0a10ecbac6dc9bfdf7693ae2
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-17 11:52:50 -0600

    adding mainlayout code behind. Needs impl

---
+++ DevDashboard/Shared/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace DevDashboard.Shared
+{
+    public partial class MainLayout : ComponentBase
+    {
+        const string message = "null reply"; //TO DO: Find out why Invoking JS Requires input? 
+
+        private string[] args = { message };
+
+        private async Task Alert()
+        {
+            await JS.InvokeAsync<string>("showAbout", args); //This is an easy way to call JS method
+        }
+    }
+}
