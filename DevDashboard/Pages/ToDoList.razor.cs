6a92f6914da1d10db8bc3a6d7c7d3919d6a6647e
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-22 11:21:23 -0600

    Adding to do list by panels.
    splitting out into own component.

--- DevDashboard/Pages/ToDoList.razor.cs
+++ DevDashboard/Pages/ToDoList.razor.cs
@@ -5,13 +5,18 @@ using DevDashboard.Models;
 namespace DevDashboard.Pages
 {
     public partial class ToDoList : ComponentBase
-    { 
+    {
         public enum ToDoStatus { Open, Complete, Withdrawn }
-        private ToDoItem[]? toDoItems;
+        private ToDoItem[]? Items_All, Items_ToDo, Items_InProg, Items_Done;
 
         protected override async Task OnInitializedAsync()
         {
-            toDoItems = await Http.GetFromJsonAsync<ToDoItem[]>("sample-data/todo.json");
+            Items_All = await Http.GetFromJsonAsync<ToDoItem[]>("sample-data/todoItems.json");
+            if (Items_All is null) { return; }
+
+            Items_ToDo = Items_All.Where(x => x.Status == "To Do").ToArray();
+            Items_InProg = Items_All.Where(x => x.Status == "In Progress").ToArray();
+            Items_Done = Items_All.Where(x => x.Status == "Done").ToArray();
         }
 
         public void AddToDoItem()
@@ -35,5 +40,9 @@ namespace DevDashboard.Pages
             // ToDo: Possibly link to a Notification Service that dynamically builds the emails.
             // Ideally code should match CodeReviews, and be a simple method call.
         }
+
+
+
+
     }
 }
